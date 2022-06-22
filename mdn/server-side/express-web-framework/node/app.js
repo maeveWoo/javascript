94d548fc7123e57f4a94c77d3ee795b5f432d433
maeve <maeve.woo@kakao.com>
2022-06-22 18:00:13 +0900

    module export/import

--- mdn/server-side/express-web-framework/node/app.js
+++ mdn/server-side/express-web-framework/node/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const square = require('./square');
 const app = express();
 const port = 3000;
 
 app.get('/', function(req, res) {
+    console.log('The area of a square with a width of 4 is ' + square.area(4));
     res.send('Hello World!');
 });
 
